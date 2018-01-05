e7d4b36f71031d41514c453734e46db79ddcacfe
Alexander A. Klimov <grandmaster@al2klimov.de>
2018-01-05 22:16:10 +0100

    Load config

--- wot-autosign-4puppet.js
+++ wot-autosign-4puppet.js
@@ -4,3 +4,22 @@
 if (module !== require.main) {
     throw new Error("RTFM");
 }
+
+const config = require("./lib/config");
+
+
+process.on("unhandledRejection", reason => {
+    throw reason;
+});
+
+(async () => {
+    let configs = await config.load(process.argv.slice(2));
+
+    if (! configs.length) {
+        throw new Error("Nothing to do");
+    }
+
+    console.log(JSON.stringify(configs));
+})().catch(err => {
+    throw err;
+});
